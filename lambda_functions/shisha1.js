a1dbad682b8cfc67270fde027834c8355855a9a5
earllee333 <jerrykiet2012@gmail.com>
2021-05-19 15:23:13 +0800

    09

--- lambda_functions/shisha1.js
+++ lambda_functions/shisha1.js
@@ -34,30 +34,12 @@ const queryDatabase = async (db) => {
     body: JSON.stringify(data),
   };
 };
-const pushToDatabase = async(db,data)=>{
-    const MyData = {
-        name:data.name,
-        email:data.email,
-        phone:data.phone,
-        number:data.number,
-    };
-    if(MyData.name && MyData.email){
-        await db.collection('notes').insertMany([data]);
-        return{statusCode:201};
 
-    }   else{
-        return{statusCode:422}
-    }
+module.exports.handler = async (event, context) => {
+  // otherwise the connection will never complete, since
+  // we keep the DB connection alive
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  const db = await connectToDatabase(MONGODB_URI);
+  return queryDatabase(db);
 };
-module.exports.handler=async (event,context)=>{
-  const db =await connectToDatabase(MONGODB_URI)
-
-  switch(event.httpMethod){
-      case "GET":
-          return queryDatabase(db);
-      case "POST":
-          return pushToDatabase(db,JSON.parse(event.body))
-      default:
-          return{statusCode:400}
-  }
-}
