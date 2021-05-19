99ea4efa24af2fdcc0b8a27cd2c5df222b7a0605
earllee333 <jerrykiet2012@gmail.com>
2021-05-20 07:34:35 +0800

    110

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -2,14 +2,24 @@ import {useState,useEffect}from 'react'
 import{useRouter} from 'next/router';
 import{Confirm,Button,Loader} from 'semantic-ui-react';
 
-const Note = ({note}) => {
-    console.log(note)
+const Note = async({query:{id}}) => {
+    const [myData,seMyData] = useState(null)
+    useEffect(async()=>{
+        console.log('Effect call')
+        const res = await fetch('/.netlify/functions/customer')
+        const data = await res.json()
+        setMyData(data)
+        console.log('Data= ',JSON.stringify(data))
+    },[])
     
-    
+    if (!myData.name){
+        return ( <div><h2>Lodaing</h2></div>)
+    }
     return ( 
         
      <div>
-         <h1>hi</h1>
+         {myData.name&&<div>{myData.name}</div>}
+         {!myData.name&& <div>Nothing</div>}
      </div>
     )
 }
