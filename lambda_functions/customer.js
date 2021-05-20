d56a028eaa61bacc8f0c4bb69d38bd7c982f7aba
earllee333 <jerrykiet2012@gmail.com>
2021-05-20 14:08:08 +0800

    ppy

--- lambda_functions/customer.js
+++ lambda_functions/customer.js
@@ -1,10 +1,10 @@
-import Note from '../models/Note'
 const MongoClient = require("mongodb").MongoClient;
 
 const MONGODB_URI = process.env.MONGODB_URI;
 const DB_NAME = '1992shisha';
 
 let cachedDb = null;
+const getId = (urlPath) => urlPath.match(/([^/]*)\/*$/)[0];
 
 
 const connectToDatabase = async (uri) => {
@@ -21,10 +21,10 @@ const connectToDatabase = async (uri) => {
   return cachedDb;
 };
 
-const queryDatabase = async (ss,db) => {
+const queryDatabase = async (db,id) => {
   const data = await 
         db.collection("notes")
-        .findOne({_id:ss})
+        .findOne(id)
 
   return {
     statusCode: 200,
@@ -34,12 +34,14 @@ const queryDatabase = async (ss,db) => {
     body: JSON.stringify(data),
   };
 };
-module.exports.handler=async (req,res,event,context)=>{
+module.exports.handler=async (event,context)=>{
   const db =await connectToDatabase(MONGODB_URI)
-  const ss = req.params.id
+  const {path} = event
+  const id = getId(path)
+  console.log(id)
   switch(event.httpMethod){
       case "GET":
-          return queryDatabase(db,ss);
+          return queryDatabase(db,id);
      default:
           return{statusCode:400}
   }
