719d7afe21b896beb3516f80a52b36419ac6927a
earllee333 <jerrykiet2012@gmail.com>
2021-05-20 16:08:55 +0800

    pvp

--- lambda_functions/customer1.js
+++ lambda_functions/customer1.js
@@ -23,7 +23,7 @@ const queryDatabase = async (db) => {
   const data = await 
         db.collection('notes')
         //.findOne({'name':'33Q'})
-        .find({"_id":"ObjectId"("60a397f6fb93f300081a6b5c")})
+        .find({"_id":"ObjectId(60a397f6fb93f300081a6b5c)"})
   return {
     statusCode: 200,
     headers: {
