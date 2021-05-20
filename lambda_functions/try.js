af3aca11049b563b48908faf4d6dabc05a261ee2
earllee333 <jerrykiet2012@gmail.com>
2021-05-20 11:31:59 +0800

    v12

--- lambda_functions/try.js
+++ lambda_functions/try.js
@@ -1,29 +1,48 @@
-const { connectToDatabase } = require("../util/mongodb");
+const MongoClient = require("mongodb").MongoClient;
 
-const pushToDatabase = async(db,data)=>{
-    const MyData = {
-        name:data.name,
-        email:data.email,
-        phone:data.phone,
-        number:data.number,
-    };
-    if(MyData.name && MyData.email){
-        await db.collection('notes').insertMany([data]);
-        return{statusCode:201};
+const MONGODB_URI = process.env.MONGODB_URI;
+const DB_NAME = '1992shisha';
 
-    }   else{
-        return{statusCode:422}
-    }
+let cachedDb = null;
+const getId = (urlPath) => urlPath.match(/([^/]*)\/*$/)[0];
+
+
+const connectToDatabase = async (uri) => {
+  // we can cache the access to our database to speed things up a bit
+  // (this is the only thing that is safe to cache here)
+  if (cachedDb) return cachedDb;
+
+  const client = await MongoClient.connect(uri, {
+    useUnifiedTopology: true,
+  });
+
+  cachedDb = client.db(DB_NAME);
+
+  return cachedDb;
+};
+
+const queryDatabase = async (db,id) => {
+  const data = await 
+        db.collection("notes")
+        .findById({_id:"60a4be28a27d180009c5d72d"})
+        .toArray()
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  };
 };
 module.exports.handler=async (event,context)=>{
-    const db =await connectToDatabase(MONGODB_URI)
-
-    switch(event.httpMethod){
-        case "GET":
-            return queryDatabase(db);
-        case "POST":
-            return pushToDatabase(db,JSON.parse(event.body))
-        default:
-            return{statusCode:400}
-    }
+  const db =await connectToDatabase(MONGODB_URI)
+  const {path} = event
+  const id = getId(path)
+  switch(event.httpMethod){
+      case "GET":
+          return queryDatabase(db,id);
+     default:
+          return{statusCode:400}
+  }
 }
