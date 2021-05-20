9b961b747816326fc6c13f6eda522268226c5ec6
earllee333 <jerrykiet2012@gmail.com>
2021-05-20 11:15:54 +0800

    pp

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -2,19 +2,10 @@ import {useState,useEffect}from 'react'
 import{useRouter} from 'next/router';
 import{Confirm,Button,Loader} from 'semantic-ui-react';
 
-const Note = () => {
-    const [myData,setMyData] = useState(null)
-    useEffect(async({query:{id}})=>{
-        console.log('Effect call')
-        const res = await fetch('/.netlify/functions/customer')
-        const data = await res.json()
-        setMyData(data)
-        console.log('Data= ',JSON.stringify(data))
-    },[])
+const Note = ({note}) => {
+    console.log(note)
     
-    if (!myData){
-        return ( <div><h2>Lodaing</h2></div>)
-    }
+    
     return ( 
         
      <div>
@@ -24,4 +15,10 @@ const Note = () => {
 }
  
 export default Note;
+
+Note.getInitialProps = async ({query:{id}})=>{
+    const res = await fetch('/.netlify/functions/customer')
+    const {data}= await res.json();
+    return{note:data}
+}
 
